8b84df53497be34e16011ecc59da4e587d9a4c25
mariabeatrizsc <mabeatriz.escotec@gmail.com>
2024-01-15 17:22:13 -0300

    desafio dio

---
+++ outro/index.py
@@ -0,0 +1,26 @@
+nome=input()
+xp=int(input())
+if xp<=1000:
+  xp='Ferro'
+  print("O Herói de nome",nome,"está no nível de",xp)
+elif xp >=1001 and xp<=2000:
+  xp='Bronze'
+  print("O Herói de nome",nome,"está no nível de",xp)
+elif xp >=2001 and xp<=5000:
+  xp='Prata'
+  print("O Herói de nome",nome,"está no nível de",xp)
+elif xp >=5001 and xp<=7000:
+  xp='Ouro'
+  print("O Herói de nome",nome,"está no nível de",xp)
+elif xp >=7001 and xp<=8000:
+  xp='Platina'
+  print("O Herói de nome",nome,"está no nível de",xp)
+elif xp >=8001 and xp<=9000:
+  xp='Ascedente'
+  print("O Herói de nome",nome,"está no nível de",xp)
+elif xp >=9001 and xp<=10000:
+  xp='Imortal'
+  print("O Herói de nome",nome,"está no nível de",xp)
+elif xp>=10001:
+  xp='Radiante'
+  print("O Herói de nome",nome,"está no nível de",xp)
